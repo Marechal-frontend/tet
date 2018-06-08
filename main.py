17cf132a5e343bd4bead0087fc583912b8819aa4
Marechal <39668525+Marechal-frontend@users.noreply.github.com>
2018-06-08 18:52:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,3 @@ async def on_ready():
 async def on_message(message):
 	if message.content.lower().startswith('t!'):
 		await client.send_message(message.chanel, "Tet bot")
-
-client.run('NDU0Njk2NzA4ODk5NzMzNTE4.Dfx04g.Z37eo79wsKUoX5RzxhQmZckP5mY')
